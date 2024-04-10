6d6d86627f90b38c4c2072de2ce52e69a8114b3c
phornchai Hompromma <164604416+PhornchaiP-H@users.noreply.github.com>
2024-04-11 05:45:12 +0700

    laravel_React

---
+++ react-laravel/backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image',
+        ]);
+
+        try {
+            $imageName = Str::random() . '.' . $request->image->getClientOriginalExtension();
+            Storage::disk('public')->putFileAs('product/image', $request->image, $imageName);
+            Product::create($request->post() + ['image' => $imageName]);
+
+            return response()->json([
+                'message' => 'Product created successfully!'
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something foes wrong while creating a Product!'
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ react-laravel/frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Container, Row, Col } from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import ProductList from './components/productList';
+import CreateProduct from './components/CreateProduct';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Navbar bg='primary'>
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            React & Laravel CRUD APP
+          </Link>
+        </Container>
+      </Navbar>
+      <Container className='mt-5'>
+        <Col md={12}>
+          <Routes>
+            <Route exact path="/" element={<ProductList />} />
+            <Route path="/product/create" element={<CreateProduct />} />
+          </Routes>
+        </Col>
+      </Container>
+
+    </Router>
+       
+  )
+}
+
+export default App
